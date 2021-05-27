b3408eea1d8be654150945449f4b7e0727f8217f
dimitry <dimabymerang0@gmail.com>
2021-05-27 13:19:53 +0300

    https fix

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "https://dimahoperskiy:8443/",
+    baseURL: "https://dimahoperskiy.ru:8443/",
     withCredentials: true
 })
 
